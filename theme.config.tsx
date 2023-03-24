fb740c1b4217a6266870f0c406d82665d949e9f0
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-03-24 14:42:34 +0800

    chore: 增加信息

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-
+const Logo = <>
+  <img alt="vcdp logo" src="https://image.liuyongzhi.cn/vcdp/favicon.ico" width={20} height={20} style={{ borderRadius: '50%' }} />
+  <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+        Vcdp
+  </span>
+  </>
+const Head = <>
+  <link rel="icon" href="https://image.liuyongzhi.cn/vcdp/favicon.ico" />
+</>
 const config: DocsThemeConfig = {
-  logo: <span>Vcdp</span>,
+  logo: Logo,
+  head: Head,
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – VCDP'
+    }
+  },
   project: {
     link: 'https://github.com/overdev-l/vcdp',
   },
   docsRepositoryBase: 'https://github.com/overdev-l/vcdpdocs',
+  banner: {
+    text: "Vcdp 目前还处在开发阶段"
+  },
   footer: {
     text: '',
   },
